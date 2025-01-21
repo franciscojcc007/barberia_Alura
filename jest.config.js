85a0157fa76b11fcafe5ed755ce14a56fbab2199
franciscoj <frankmazta@gmail.com>
2025-01-21 17:57:04 -0400

    se añadió archivo de configuración de Jest para establecer el entorno de pruebas y definir patrones de archivos de prueba

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: 'node',
+  testRegex: '(/__tests__/.*|(\\.|/))(test|spec)\\.(jsx?|tsx?)$',
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+};
